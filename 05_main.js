06dffff2f0458e703c0cc98c10728d658d4e058c
[apalatnik] <[s.palatnik@gmx.de]>
2019-07-12 16:43:26 +0200

    withoutID

--- 05_main.js
+++ 05_main.js
@@ -28,7 +28,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "73",
-            serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/73",
+            serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
