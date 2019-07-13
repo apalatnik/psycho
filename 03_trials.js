aff25ec505e4baca553bc66f5d29eb65015a0496
[apalatnik] <[s.palatnik@gmx.de]>
2019-07-13 18:11:47 +0200

    fixed trials

--- 03_trials.js
+++ 03_trials.js
@@ -28,367 +28,367 @@ const trial_info = {
   ],
   rating_scale:[
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/001.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/002.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/003.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/004.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/005.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/006.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/007.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/008.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/009.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/106.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/010.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/011.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/012.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/013.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/014.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/015.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/036.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/037.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/038.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/039.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/040.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/041.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/042.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/043.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/044.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/045.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/046.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/047.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/048.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/049.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/050.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/072.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/073.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/074.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/075.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/105.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/076.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/077.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/078.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/079.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/080.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/081.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/104.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/082.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/083.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/084.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/085.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/086.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/103.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/087.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/092.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/093.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/102.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/094.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/095.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/101.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/096.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/097.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/098.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/099.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
       },
       {
-          question: "Rate this picture",
+          question: "<br /><br /><br /><br />Rate this picture",
           picture: 'realartsyimages/100.jpg',
           optionLeft: 'not at all',
           optionRight: 'very'
